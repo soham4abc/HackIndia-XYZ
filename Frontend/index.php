23ef724370fae850177066a6651f72f074e25118
CoderRounak <rounakhazra999@gmail.com>
2022-03-25 00:27:56 +0530

    made the navbar using bootstrap

---
+++ Frontend/index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>jpg to txt</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link href="" rel="icon">
+  <link href="" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <!-- Tailwind CSS -->
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <!-- <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+      <a href="#hero" class="logo"><img src="PicsArt_12-11-06.58.40.jpg" alt="logo" class="img-fluid"></a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto" href="./ocr.php">OCR</a></li>
+          <li><a class="nav-link scrollto" href="#">Notes</a></li>
+          <li><a class="nav-link scrollto" href="./custom-search.php">Custom Search</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+  </header> -->
+  <!-- End Header -->
+
+    <!-- navbar -->
+
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./index.php">
+                <img class="nav icon" src="./OCR icon.png" width="30" height="24">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="ocr.html">OCR</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Notes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="custom-search.php">Custom Search</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+
+  
+
+  <main id="main">
+
+   
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="./OCR image.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <h3><p class="fst-italic">OCR is perhaps the best features we are providing</p></h3>
+            <ul>
+              <li><i class="bi bi-check-circle"></i>No data logging. </li>
+              <li><i class="bi bi-check-circle"></i>Updated regularly to provide you with the best user experience. </li>
+            </ul>
+            <p>
+              We value your privacy as much you do. Our server is regularly inspected to keep it secure.
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          
+          <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <h3><p class="fst-italic">All notes available here, don't worry about exams.</p></h3>
+            <ul>
+              <li><i class="bi bi-check-circle"></i>No data logging. </li>
+              <li><i class="bi bi-check-circle"></i>Updated regularly to provide you with the best user experience. </li>
+            </ul>
+            <p>
+              We value your privacy as much you do. Our server is regularly inspected to keep it secure.
+            </p>
+          </div>
+          <div class="col-lg-6">
+            <img src="./Notes.png" class="img-fluid" alt="">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="./Google search.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <h3><p class="fst-italic">Tired of getting wrong google results? Try this out!</p></h3>
+            <ul>
+              <li><i class="bi bi-check-circle"></i>No data logging. </li>
+              <li><i class="bi bi-check-circle"></i>Updated regularly to provide you with the best user experience. </li>
+            </ul>
+            <p>
+              We value your privacy as much you do. Our server is regularly inspected to keep it secure.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+
+
+     <!-- ======= Team Section ======= -->
+<!-- <section id="team" class="team section-bg">
+    <div class="container">
+
+      <div class="section-title">
+        <span>Team</span>
+        <h2>Team</h2>
+        <p>We are a team of computer enthusiasts exploring new opportunities.</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+          <div class="member">
+            <img src="IMG-20210928-WA0007.jpg" alt="Soham Banerjee">
+            <h4>Soham Banerjee</h4>
+            <span>Backend Developer</span>
+            <p>A not so average coder trying to explore oppurtunities.</p>
+            <div class="social">
+              <a target="_blank" href="https://www.instagram.com/debugginglife_02"><i class="bi bi-instagram"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/soham-banerjee-6091831b3/"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+          <div class="member">
+            <img src="IMG20191028160454.jpg" alt="Subhadip Dutta">
+            <h4>Subhadip Dutta</h4>
+            <span>Frontend Developer</span>
+            <p>An aspiring coder looking forward to learn new things.</p>
+            <div class="social">
+              <a target="_blank" href="https://www.instagram.com/lol_sudu"><i class="bi bi-instagram"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/subhadip-dutta-32b7551b4/"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+          <div class="member">
+            <img src="ssir.jpeg" alt="Sumon Sen">
+            <h4>Sumon Sen</h4>
+            <span>Guide &amp; Server maintainance </span>
+            <p>An experienced coder who guided his sudents with all that they needed.</p>
+            <div class="social">
+              <a target="_blank" href="https://www.instagram.com/im_sumon"><i class="bi bi-instagram"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/sumonsenind/"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>End Team Section -->
+
+  
+    
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>2022</span></strong>. All Rights Reserved. By Team -<strong> XYZ </strong>.
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
