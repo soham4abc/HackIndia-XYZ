ded6be4a044bfe4b950f818e62737cafacccc078
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2022-03-24 13:36:55 +0530

    backend initiated

---
+++ Backend/API/scripts/ocr2.py
@@ -0,0 +1,37 @@
+import cv2
+import os, argparse
+import pytesseract
+from PIL import Image
+import docx
+import aspose.words as aw
+
+
+def image_to_text(path):
+
+    images = cv2.imread(path)
+
+    gray = cv2.cvtColor(images, cv2.COLOR_BGR2GRAY)
+
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, gray)
+    text = pytesseract.image_to_string(Image.open(filename))
+    os.remove(filename)
+
+    finaltext = "".join(c for c in text if valid_xml_char_ordinal(c))
+    mydoc = docx.Document()
+
+    mydoc.add_paragraph(finaltext)
+    mydoc.save("../../Frontend/documents/MyFile.docx")
+
+    cv2.waitKey(0)
+
+
+def valid_xml_char_ordinal(c):
+    codepoint = ord(c)
+
+    return (
+        0x20 <= codepoint <= 0xD7FF
+        or codepoint in (0x9, 0xA, 0xD)
+        or 0xE000 <= codepoint <= 0xFFFD
+        or 0x10000 <= codepoint <= 0x10FFFF
+    )
---
+++ Backend/API/scripts/test2.py
@@ -0,0 +1,17 @@
+import base64
+from pickletools import optimize
+from PIL import Image
+from io import BytesIO
+from scripts import ocr2
+
+
+def text_to_image(datatext):
+    datastr = (datatext.data).values
+    datastr[0] = datastr[0].split(",", 1)
+
+    data = datastr[0][1]
+
+    im = Image.open(BytesIO(base64.b64decode(data)))
+
+    im.save("image.png", "PNG")
+    ocr2.image_to_text("image.png")
