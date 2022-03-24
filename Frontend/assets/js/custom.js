e8ed1f932d8b4fd540045de1c03402bb1d682a03
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2022-03-24 15:44:37 +0530

    Initial commit

---
+++ Frontend/assets/js/custom.js
@@ -0,0 +1,116 @@
+// url="http://13.234.136.230:81/"
+url="https://ocr-api.findcodes.xyz/"
+//url="http://192.168.43.6:81/"
+$(document).ready(function() {
+    
+    // $(".chosen-select").chosen({
+    //     no_results_text: "Oops, nothing found!"
+    // });
+    // $("#messagebox").hide();
+    // $("#messagebox2").hide();
+    // $("data").appendTo('#messagebox1'); 
+    $.ajax({
+        type: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/todos/1',
+        headers: {}
+        //OR
+        //beforeSend: function(xhr) { 
+        //  xhr.setRequestHeader("My-First-Header", "first value"); 
+        //  xhr.setRequestHeader("My-Second-Header", "second value"); 
+        //}
+    }).done(function(data) { 
+        console.log(data);
+        // $.each(data.records,function(i,obj)
+        //         {
+        //         //  alert(obj.value+":"+obj.text);
+        //          var div_data="<option value="+obj.id+">"+obj.fields.Name+"</option>";
+        //         // alert(div_data);
+        //         $(div_data).appendTo('#select_property'); 
+        //         });
+    });
+  
+});
+
+$(document).ready(function () {
+
+    $("#Submit").submit(function (e) {
+
+                
+    e.preventDefault();
+
+    $("#btnSubmit").attr("disabled", true);
+
+  return true;
+
+});
+});
+
+function functionmyfunction() {   
+    var file = $('#UnprocessedImage').prop('files');
+    console.log(file);
+    // return;
+    const toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+    
+    async function Main() {
+      const file = document.querySelector('#UnprocessedImage').files[0];
+    //   console.log(JSON.stringify({
+    //     "data":await toBase64(file),
+    // }));
+      ajaxCall('users', await toBase64(file));
+    }
+    
+    
+
+    Main();  
+}  
+
+function ajaxCall(urlEndPoint, data) {
+    $.ajax({
+        type: 'POST',
+        url: url + urlEndPoint,
+    headers: {},
+        dataType: 'json',
+        data:JSON.stringify({
+            "data":data
+        }),
+        processData: false,
+        contentType: "application/json",
+        //OR
+        //beforeSend: function(xhr) { 
+        //  xhr.setRequestHeader("My-First-Header", "first value"); 
+        //  xhr.setRequestHeader("My-Second-Header", "second value"); 
+        //}
+    }).done(function(data) { 
+        console.log(data);
+        download(data.data, 'yourfile.docx');
+        // browser.downloads.download({url: data.data})
+        console.log(1)
+        // $.each(data.records,function(i,obj)
+        //         {
+        //         //  alert(obj.value+":"+obj.text);
+        //          var div_data="<option value="+obj.id+">"+obj.fields.Name+"</option>";
+        //         // alert(div_data);
+        //         $(div_data).appendTo('#select_property'); 
+        //         });
+    });
+}
+
+function download(url, filename) {
+    fetch(url).then(function(t) {
+        return t.blob().then((b)=>{
+            var a = document.createElement("a");
+            a.href = URL.createObjectURL(b);
+            a.setAttribute("download", filename);
+            a.click();
+        }
+        );
+    });
+
+    $("#btnSubmit").attr("enabled", true);
+    }
+
---
+++ Frontend/ocr.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>jpg to txt</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link href="" rel="icon">
+  <link href="" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+      <a href="#hero" class="logo"><img src="PicsArt_12-11-06.58.40.jpg" alt="logo" class="img-fluid"></a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <form id="Submit" action="#" method="POST" class="formSegments">
+          <div class="form-group mb-2">
+            <label for="UnprocessedImage"><h2>Upload Image Here</h2></label>       
+          </div>
+          <div class="form-group mb-2">
+            <input type="file" class="form-control-file" id="UnprocessedImage">
+          </div>
+          <div class="form-group mb-2">
+            <button type="button" class="btn btn-primary" onclick = "functionmyfunction()" id="btnSubmit" value="Submit">Submit</button>
+          </div>
+        </form>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src="istockphoto-1289388658-612x612-removebg-preview.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+   
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="assets/img/about.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <h3><p class="fst-italic">Why use our service at the first place?</p></h3>
+            <ul>
+              <li><i class="bi bi-check-circle"></i>No data logging. </li>
+              <li><i class="bi bi-check-circle"></i>Updated regularly to provide you with the best user experience. </li>
+            </ul>
+            <p>
+              We value your privacy as much you do. Our server is regularly inspected to keep it secure.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+
+
+     <!-- ======= Team Section ======= -->
+<section id="team" class="team section-bg">
+    <div class="container">
+
+      <div class="section-title">
+        <span>Team</span>
+        <h2>Team</h2>
+        <p>We are a team of computer enthusiasts exploring new opportunities.</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+          <div class="member">
+            <img src="IMG-20210928-WA0007.jpg" alt="Soham Banerjee">
+            <h4>Soham Banerjee</h4>
+            <span>Backend Developer</span>
+            <p>A not so average coder trying to explore oppurtunities.</p>
+            <div class="social">
+              <a target="_blank" href="https://www.instagram.com/debugginglife_02"><i class="bi bi-instagram"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/soham-banerjee-6091831b3/"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+          <div class="member">
+            <img src="IMG20191028160454.jpg" alt="Subhadip Dutta">
+            <h4>Subhadip Dutta</h4>
+            <span>Frontend Developer</span>
+            <p>An aspiring coder looking forward to learn new things.</p>
+            <div class="social">
+              <a target="_blank" href="https://www.instagram.com/lol_sudu"><i class="bi bi-instagram"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/subhadip-dutta-32b7551b4/"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+          <div class="member">
+            <img src="ssir.jpeg" alt="Sumon Sen">
+            <h4>Sumon Sen</h4>
+            <span>Guide &amp; Server maintainance </span>
+            <p>An experienced coder who guided his sudents with all that they needed.</p>
+            <div class="social">
+              <a target="_blank" href="https://www.instagram.com/im_sumon"><i class="bi bi-instagram"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/in/sumonsenind/"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section><!-- End Team Section -->
+
+  
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <span>Contact</span>
+          <h2>Contact</h2>
+          <p></p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Kolkata, India</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>csenthusiasts@gmail.com</p>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="form-group col-md-6 mt-3 mt-md-0">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject" id="subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>2021</span></strong>. All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+</body>
+
+</html>
